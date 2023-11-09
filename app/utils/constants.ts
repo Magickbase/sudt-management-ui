5e4448e51cf5640c321ee518f8c3ec97da2e7516
Keith <keithwhisper@gmail.com>
2023-11-09 15:55:22 +0900

    refactor: use sdk instead of connecting to neuron manually

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,3 +1,6 @@
+import type { Network } from '@ckb-connect/walletconnect-dapp-sdk'
 export const WC_ID = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
 export const CODE_HASH_LIST =
   process.env.NEXT_PUBLIC_CODE_HASH_LIST?.split(',') ?? []
+export const NETWORK: Network =
+  (process.env.NEXT_PUBLIC_NETWORK as Network) ?? 'testnet'
